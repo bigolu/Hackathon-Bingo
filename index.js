e0ef899f60774b568839e9166019e81b532d006a
biggie96 <biggieemmanuel@gmail.com>
2015-06-27 22:56:09 -0400

    added documentation

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-	console.log($("td").css("background-color"));
-
-$(document).ready(function(){
-	$("td").click(function () {
+/* This function handle the clicking of buttons on the page*/
+function start_game(){
+	$("td").click(function () { //Toggles color between white and yellow
 		if($(this).css("background-color") === 'rgb(255, 255, 255)'){
    			 $(this).css("background-color","yellow");
    		}
@@ -10,78 +9,76 @@ $(document).ready(function(){
    		}
 
    		var table = document.getElementById("table");
-
-   		if(youWon(table)){
+   		if(youWon(table)){ //Check to see if they won
    			winner();
    		}
-	});	
-});
+	});
+}
 
-
-
-
-
-
-
-
+/* Shows the page for winners */
 function winner(){
-
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC", true);
+xhr.open("GET", "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC", true); //get a random gif
 xhr.onreadystatechange = function() {
   if (xhr.readyState == 4) {
+		/* Change background to a gif*/
   	var gif = jQuery.parseJSON(xhr.responseText);
   	var url = gif.data.image_url;
   	var bg = "url(" + url + ")";
-  	
+		document.body.style.backgroundImage = bg;
+
+		/* Display you win in big letters */
   	document.body.innerHTML = "<h1>YOU WIN!!!!!</h1>";
-  	$('h1').css({
-  	fontSize: '200px',
-  	color: 'red',
-    left: '0',
-    lineHeight: '200px',
-    margin: 'auto',
-    marginTop: '-100px',
-    position: 'absolute',
-    top: '50%',
-    width: '100%',
-	});
-    document.body.style.backgroundImage = bg;
-    window.setInterval(function test(){ 
+	  	$('h1').css({
+	  	fontSize: '200px',
+	  	color: 'red',
+	    left: '0',
+	    lineHeight: '200px',
+	    margin: 'auto',
+	    marginTop: '-100px',
+	    position: 'absolute',
+	    top: '50%',
+	    width: '100%',
+		});
+
+		/* Make text on screen flash different colors by changing the color every 1/10 of a second*/
+    window.setInterval(function (){
+				/* generate random color by randomizing a hexadecimal */
     		var letters = '0123456789ABCDEF'.split('');
-    var color = '#';
-    for (var i = 0; i < 6; i++ ) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-
-    $('h1').css("color", color);
-
-     }, 100);   
-
-
-
+		    var color = '#';
+		    for (var i = 0; i < 6; i++ ) {
+		        color += letters[Math.floor(Math.random() * 16)];
+		    }
+		    $('h1').css("color", color);
+			}
+  	, 100);
   }
 }
-xhr.send();
-
+xhr.send(); //send request to giphy
 }
 
-
+/* Returns true if the player has won the game
+* by checking for five in a row anywhere on the board
+*/
 function youWon(table){
-	for(i = 0; i < table.rows.length; i++){
-		var count = 0;
-		for(j = 0; j < table.rows.length; j++){
-			if($(table.rows[i].cells[j]).css("background-color") != 'rgb(255, 255, 255)')
+	var count = 0; //keep track of how many  the user got in a row
 
+	/* Check for horizontal win */
+	for(i = 0; i < table.rows.length; i++){
+		count = 0; //reset count
+		for(j = 0; j < table.rows.length; j++){
+			if($(table.rows[i].cells[j]).css("background-color") != 'rgb(255, 255, 255)'){
 				count++;
+			}
 		}
 		if(count === 5){
 			return true;
 		}
 	}
 
+	/* Check for vertical win */
 	for(i = 0; i < table.rows.length; i++){
-		var count = 0;
+		count = 0;
 		for(j = 0; j < table.rows.length; j++){
 			if($(table.rows[j].cells[i]).css("background-color") != 'rgb(255, 255, 255)')
 				count++;
@@ -91,7 +88,8 @@ function youWon(table){
 		}
 	}
 
-	var count = 0;
+	/* Check for diagonal win from left to right */
+	count = 0;
 	for(i = 0, j = 0; (i < table.rows.length) && (j < table.rows.length); i++, j++){
 			if($(table.rows[j].cells[i]).css("background-color") != 'rgb(255, 255, 255)')
 				count++;
@@ -100,7 +98,8 @@ function youWon(table){
 		return true;
 	}
 
-	var count = 0;
+	/* Check for diagonal win from right to left */
+	count = 0;
 	for(i = table.rows.length - 1, j = 0; (i >= 0) && (j < 5); i--, j++){
 			if($(table.rows[j].cells[i]).css("background-color") != 'rgb(255, 255, 255)'){
 				count++;
@@ -109,18 +108,8 @@ function youWon(table){
 	if(count === 5){
 		return true;
 	}
+
+	return false;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+$(document).ready(start_game);
---
+++ readme.md
@@ -0,0 +1,8 @@
+#Hackathon Bingo
+######A bingo game with tiles relating to things you'll probably see at a Hackathon.
+
+If you're ever at a hackathon and your code just refuses to run, you can relax with a quick game of hackBingo!
+
+##[Preview 1](https://raw.githubusercontent.com/biggie96/Hackathon-Bingo/master/prev1.png)
+
+##[Preview 2](https://raw.githubusercontent.com/biggie96/Hackathon-Bingo/master/prev2.png)
